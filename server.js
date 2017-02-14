b9f41767d18b604025cb1775f782df1b9e650971
Maicon <maicon-gil@PRO-PAE-0483.PROCERGS.REDERS>
2017-02-14 17:20:38 -0200

    Add public directory with index.html

--- server.js
+++ server.js
@@ -1,11 +1,22 @@
 var express = require('express'),
     multer  = require('multer'),
+    path = require('path'),
     upload = multer({ dest: 'uploads/' }),
     app = express(),
     port = process.env.PORT || 8000;
  
+
+app.use(express.static(path.join(__dirname, 'public')));
+
 app.get("/", function(req, res){
-    res.send("OK");
+    res.sendFile("index.html");
+});
+
+app.post("/upload", upload.array(), function(req, res){
+//User Story: I can submit a FormData object that includes a file upload.
+//User Story: When I submit something, I will receive the file size in bytes within the JSON response
+    console.log(req.body);
+    res.send("UPLOAD");
 });
 
 app.listen(port, function(){
